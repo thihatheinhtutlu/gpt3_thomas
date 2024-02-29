0278e64690973ca1e20dc4618aa588573d0a0bfa
thihatheinhtutlu <linae0343@gmail.com>
2024-02-29 23:09:39 +0630

    updated wgpt3 component as well as feature component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Brand.css';
+import { google, slack, shopify, dropbox, assian } from './import';
 
 const Brand = () => {
   return (
-    <div>
-        Brand
+    <div className='gpt3__brand section__padding'>
+        <div>
+          <img src={google} alt='google'/>
+        </div>
+
+        <div>
+          <img src={slack} alt='slack'/>
+        </div>
+
+        <div>
+          <img src={assian} alt='assian'/>
+        </div>
+
+        <div>
+          <img src={dropbox} alt='dropbox'/>
+        </div>
+        
+        <div>
+          <img src={shopify} alt='shopify'/>
+        </div>
     </div>
   )
 }
---
+++ src/components/brand/import.js
@@ -0,0 +1,13 @@
+import google from '../../assets/images/google.png';
+import slack from '../../assets/images/slack.png';
+import assian from '../../assets/images/atlassian.png';
+import dropbox from '../../assets/images/dropbox.png';
+import shopify from '../../assets/images/shopify.png';
+
+export {
+    google,
+    shopify,
+    slack,
+    assian,
+    dropbox
+}
